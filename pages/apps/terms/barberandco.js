4b4fb520d3e656b75a984755ee00659c2db8d1c4
Michael Rausch <me@michaelrausch.nz>
2021-04-21 17:37:29 +1200

    Fixed barber & Co CSS

--- pages/apps/terms/barberandco.js
+++ pages/apps/terms/barberandco.js
@@ -58,49 +58,50 @@ export default function Home() {
                     <p className="pb-4">
                         We keep your information for as long as you are using this service, you may request we delete all information linked to your account by emailing support@michaelrausch.nz</p>
                     <p className="pb-4">
-                        You have the right to ask for a copy of any personal information we hold about you, and to ask for it to be corrected if you think it is wrong. If you’d like to ask for a copy of your information, or to have it corrected, please contact us at support@michaelrausch.nz.</p></p>
+                        You have the right to ask for a copy of any personal information we hold about you, and to ask for it to be corrected if you think it is wrong. If you’d like to ask for a copy of your information, or to have it corrected, please contact us at support@michaelrausch.nz.</p>
+                        </p>
 
 
 
                 <h2 className="text-white text-2xl font-extrabold pb-5 pt-10">Terms And Conditions</h2>
-                <p class="text-green-100 text-sm font-semibold">
-                <p className="pb-5 ">
-                Fifth hair cut receives 50% discount and tenth haircut is free of charge. Should the customer not present their Barber & Co Loyalty Card at the time of their cut, Barber & Co will be forced to charge retail pricing or the full price advertised. Offer only applies to services with no greater value than $35. Barber & Co can withdraw this offer at any time. Not transferable nor redeemable for cash. Offer only available on submission of voucher (Loyalty Card). Change will not be given. Not to be used in conjunction with any other offer. Speak to our team for full details.</p>
-                
-                <p className="pb-5 ">
-                By downloading or using the app, these terms will automatically apply to you – you should make sure therefore that you read them carefully before using the app. You’re not allowed to copy, or modify the app, any part of the app, or our trademarks in any way. You’re not allowed to attempt to extract the source code of the app, and you also shouldn’t try to translate the app into other languages,or make derivative versions. The app itself, and all the trade marks, copyright, database rights and other intellectual property rights related to it, still belong to Barber and Co Limited.
+                <p className="text-green-100 text-sm font-semibold">
+                    <p className="pb-5 text-green-100 text-sm font-semibold">
+                        Fifth hair cut receives 50% discount and tenth haircut is free of charge. Should the customer not present their Barber & Co Loyalty Card at the time of their cut, Barber & Co will be forced to charge retail pricing or the full price advertised. Offer only applies to services with no greater value than $35. Barber & Co can withdraw this offer at any time. Not transferable nor redeemable for cash. Offer only available on submission of voucher (Loyalty Card). Change will not be given. Not to be used in conjunction with any other offer. Speak to our team for full details.</p>
+
+                        <p className="pb-5 text-green-100 text-sm font-semibold">
+                        By downloading or using the app, these terms will automatically apply to you – you should make sure therefore that you read them carefully before using the app. You’re not allowed to copy, or modify the app, any part of the app, or our trademarks in any way. You’re not allowed to attempt to extract the source code of the app, and you also shouldn’t try to translate the app into other languages,or make derivative versions. The app itself, and all the trade marks, copyright, database rights and other intellectual property rights related to it, still belong to Barber and Co Limited.
                 </p>
 
-                <p className="pb-5 ">
-                Barber and Co Limited is committed to ensuring that the app is as useful and efficient as possible. For that reason, we reserve the right to make changes to the app or to charge for its services, at any time and for any reason. We will never charge you for the app or its services without making it very clear to you exactly what you’re paying for.
+                <p className="pb-5 text-green-100 text-sm font-semibold">
+                        Barber and Co Limited is committed to ensuring that the app is as useful and efficient as possible. For that reason, we reserve the right to make changes to the app or to charge for its services, at any time and for any reason. We will never charge you for the app or its services without making it very clear to you exactly what you’re paying for.
                 </p>
 
-                <p className="pb-5 ">
-                The Barber and Co app stores and processes personal data that you have provided to us, in order to provide our Service. It’s your responsibility to keep your phone and access to the app secure. We therefore recommend that you do not jailbreak or root your phone, which is the process of removing software restrictions and limitations imposed by the official operating system of your device. It could make your phone vulnerable to malware/viruses/malicious programs, compromise your phone’s security features and it could mean that the Barber and Co app won’t work properly or at all.
+                <p className="pb-5 text-green-100 text-sm font-semibold">
+                        The Barber and Co app stores and processes personal data that you have provided to us, in order to provide our Service. It’s your responsibility to keep your phone and access to the app secure. We therefore recommend that you do not jailbreak or root your phone, which is the process of removing software restrictions and limitations imposed by the official operating system of your device. It could make your phone vulnerable to malware/viruses/malicious programs, compromise your phone’s security features and it could mean that the Barber and Co app won’t work properly or at all.
                 </p>
 
-                <p className="pb-5 ">
-                You should be aware that there are certain things that Barber and Co Limited will not take responsibility for. Certain functions of the app will require the app to have an active internet connection. The connection can be Wi-Fi, or provided by your mobile network provider, but Barber and Co Limited cannot take responsibility for the app not working at full functionality if you don’t have access to Wi-Fi, and you don’t have any of your data allowance left.
+                <p className="pb-5 text-green-100 text-sm font-semibold">
+                        You should be aware that there are certain things that Barber and Co Limited will not take responsibility for. Certain functions of the app will require the app to have an active internet connection. The connection can be Wi-Fi, or provided by your mobile network provider, but Barber and Co Limited cannot take responsibility for the app not working at full functionality if you don’t have access to Wi-Fi, and you don’t have any of your data allowance left.
                 </p>
 
-                <p className="pb-5 ">
-                If you’re using the app outside of an area with Wi-Fi, you should remember that your terms of the agreement with your mobile network provider will still apply. As a result, you may be charged by your mobile provider for the cost of data for the duration of the connection while accessing the app, or other third party charges. In using the app, you’re accepting responsibility for any such charges, including roaming data charges if you use the app outside of your home territory (i.e. region or country) without turning off data roaming. If you are not the bill payer for the device on which you’re using the app, please be aware that we assume that you have received permission from the bill payer for using the app.
+                <p className="pb-5 text-green-100 text-sm font-semibold">
+                        If you’re using the app outside of an area with Wi-Fi, you should remember that your terms of the agreement with your mobile network provider will still apply. As a result, you may be charged by your mobile provider for the cost of data for the duration of the connection while accessing the app, or other third party charges. In using the app, you’re accepting responsibility for any such charges, including roaming data charges if you use the app outside of your home territory (i.e. region or country) without turning off data roaming. If you are not the bill payer for the device on which you’re using the app, please be aware that we assume that you have received permission from the bill payer for using the app.
                 </p>
 
-                <p className="pb-5 ">
-                Along the same lines, Barber and Co Limited cannot always take responsibility for the way you use the app i.e. You need to make sure that your device stays charged – if it runs out of battery and you can’t turn it on to avail the Service, Barber and Co Limited cannot accept responsibility.
+                <p className="pb-5 text-green-100 text-sm font-semibold">
+                        Along the same lines, Barber and Co Limited cannot always take responsibility for the way you use the app i.e. You need to make sure that your device stays charged – if it runs out of battery and you can’t turn it on to avail the Service, Barber and Co Limited cannot accept responsibility.
                 </p>
 
-                <p className="pb-5 ">
-                With respect to Barber and Co Limited’s responsibility for your use of the app, when you’re using the app, it’s important to bear in mind that although we endeavour to ensure that it is updated and correct at all times, we do rely on third parties to provide information to us so that we can make it available to you. Barber and Co Limited accepts no liability for any loss, direct or indirect, you experience as a result of relying wholly on this functionality of the app.
+                <p className="pb-5 text-green-100 text-sm font-semibold">
+                        With respect to Barber and Co Limited’s responsibility for your use of the app, when you’re using the app, it’s important to bear in mind that although we endeavour to ensure that it is updated and correct at all times, we do rely on third parties to provide information to us so that we can make it available to you. Barber and Co Limited accepts no liability for any loss, direct or indirect, you experience as a result of relying wholly on this functionality of the app.
                 </p>
 
-                <p className="pb-5 ">
-                At some point, we may wish to update the app. The app is currently available on iOS – the requirements for system(and for any additional systems we decide to extend the availability of the app to) may change, and you’ll need to download the updates if you want to keep using the app. Barber and Co Limited does not promise that it will always update the app so that it is relevant to you and/or works with the iOS version that you have installed on your device. However, you promise to always accept updates to the application when offered to you, We may also wish to stop providing the app, and may terminate use of it at any time without giving notice of termination to you. Unless we tell you otherwise, upon any termination, (a) the rights and licenses granted to you in these terms will end; (b) you must stop using the app, and (if needed) delete it from your device.
+                <p className="pb-5 text-green-100 text-sm font-semibold">
+                        At some point, we may wish to update the app. The app is currently available on iOS – the requirements for system(and for any additional systems we decide to extend the availability of the app to) may change, and you’ll need to download the updates if you want to keep using the app. Barber and Co Limited does not promise that it will always update the app so that it is relevant to you and/or works with the iOS version that you have installed on your device. However, you promise to always accept updates to the application when offered to you, We may also wish to stop providing the app, and may terminate use of it at any time without giving notice of termination to you. Unless we tell you otherwise, upon any termination, (a) the rights and licenses granted to you in these terms will end; (b) you must stop using the app, and (if needed) delete it from your device.
                 </p>
 
-                <p className="pb-5 ">
-                We may update our Terms and Conditions from time to time. Thus, you are advised to review this page periodically for any changes. We will notify you of any changes by posting the new Terms and Conditions on this page.
+                <p className="pb-5 text-green-100 text-sm font-semibold">
+                        We may update our Terms and Conditions from time to time. Thus, you are advised to review this page periodically for any changes. We will notify you of any changes by posting the new Terms and Conditions on this page.
                 </p>
 
 
