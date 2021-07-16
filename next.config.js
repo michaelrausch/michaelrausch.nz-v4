16177fbd2bb1b97b814ce2f738274f6e35d3ebe4
Michael Rausch <me@michaelrausch.nz>
2021-07-16 21:33:02 +1200

    Removed serverless from next.config.js for vercel

--- next.config.js
+++ next.config.js
@@ -1,15 +1,9 @@
 const nextConfig = {
-    target: "serverless",
     webpack: function (config) {
         config.module.rules.push({
             test: /\.md$/,
             use: "raw-loader",
         });
-        config.externals = {
-            ...config.externals,
-            canvas: 'canvas',
-            critters: 'critters'
-          };
         return config;
     },
 };
