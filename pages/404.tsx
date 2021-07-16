868dca4593045a10b7ae98987e9c7385549db86d
Michael Rausch <me@michaelrausch.nz>
2021-07-17 11:07:28 +1200

    Added 404 game

--- pages/404.tsx
+++ pages/404.tsx
@@ -3,6 +3,7 @@ import { NextSeo } from "next-seo";
 import Head from "next/head";
 import { Header } from '../components/Header';
 import { Footer } from '../components/Footer';
+import ChromeDinoGame from 'react-chrome-dino';
 
 const Custom404: React.FC = () => {
     return (
@@ -15,11 +16,15 @@ const Custom404: React.FC = () => {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
 
-            <div className="bg-gray-800 h-screen">
+            <div className="bg-gray-100 h-screen">
                 <Header />
 
-                <h1 className="text-9xl font-black text-white text-center pt-32">404</h1>
-                <p className="text-5xl text-white font-bold text-center pb-96">Not Found</p>
+                <h1 className="text-9xl font-black text-gray-900 text-center pt-32">404</h1>
+                <p className="text-5xl text-gray-700 font-bold text-center pb-20">Not Found</p>
+
+                <ChromeDinoGame />
+
+                <div className="py-20"></div>
 
                 <Footer
                     name="Michael Rausch"
