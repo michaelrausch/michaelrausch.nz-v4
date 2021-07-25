34b05ee63fa7840b406e2927ba0ee6d3bd2d4db3
Michael Rausch <me@michaelrausch.nz>
2021-07-25 19:27:17 +1200

    Added parallax playground

---
+++ pages/playground/parallax.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Parallax: React.FC = () => {
+    return (
+        <>
+            <div className="bg-earth bg-no-repeat bg-center bg-cover ">
+                           
+            </div>
+        </>
+    )
+}
+
+export default Parallax;
